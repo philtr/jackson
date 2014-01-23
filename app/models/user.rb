ca967e7f63fad0cd8e73b7bddc7a072f563ef9f8
Phillip Ridlen <phillip@ovenbits.com>
2014-01-23 11:28:15 -0600

    Update user when signing in

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,15 @@ class User < ActiveRecord::Base
   end
 
   def self.authorize(auth)
-    User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.first_name ||= auth.info.first_name.presence  || auth.info.name.split(' ').first
-      user.last_name  ||= auth.info.last_name.presence   || auth.info.name.split(' ').last
-      user.email      ||= auth.info.email
-      user.avatar_url ||= auth.info.image
+    user = User.where(provider: auth.provider, uid: auth.uid).first_or_initialize
+    user.first_name ||= auth.info.first_name.presence  || auth.info.name.split(' ').first
+    user.last_name  ||= auth.info.last_name.presence   || auth.info.name.split(' ').last
+    user.email      ||= auth.info.email
+    user.avatar_url ||= auth.info.image
+    user.token      ||= auth.credentials.token
+    user.auth_hash    = auth.to_h
 
-      user.token      ||= auth.credentials.token
-
-      user.auth_hash    = auth.to_h
-    end
+    user.tap(&:save)
   end
 
   protected
