1e1bba1919fd2b5a2a8fd5e54377d72a5d92fad9
Phillip Ridlen <phillip@ovenbits.com>
2014-01-29 18:55:31 -0600

    Add some more user tests

---
+++ test/factories/responses.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :response do
+    user_id 1
+    event_id 1
+    additional_guests 0
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:responses)
+  should have_many(:events).through(:responses)
+  should have_many(:created_events).class_name("Event")
+
   context "The User class" do
     should "authorize from an auth object" do
       auth = Auth.new({ provider:    'user_test',
@@ -11,6 +15,39 @@ class UserTest < ActiveSupport::TestCase
       @user = User.authorize(auth)
 
       assert_equal true, @user.valid?
+    end
+  end
+
+  context "A user" do
+    setup do
+      @user = create(:user)
+    end
+
+    should "know if they are attending a specific event or not" do
+      assert_equal false, @user.attending?(create(:event))
+    end
+
+    should "have a gravatar" do
+      assert_match(/^http:\/\/www.gravatar.com/i, @user.gravatar)
+    end
+
+    context "A user without an email address" do
+      should "use a specified avatar if present" do
+        @user.email = nil
+        @user.avatar_url = "http://google.com"
+
+        assert_equal "http://google.com", @user.gravatar
+      end
+
+      should "use a random avatar if none specified" do
+        User.any_instance.expects(:random_avatar)
+        @user.email = nil
+        @user.avatar_url = nil
+
+        @user.gravatar
+
+        User.any_instance.unstub(:random_avatar)
+      end
     end
   end
 end
