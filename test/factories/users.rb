0c28b9fdec58c36274b4e67f06990e00cd2aee55
Phillip Ridlen <phillip@ovenbits.com>
2014-01-29 18:44:40 -0600

    Add tests for event model

---
+++ test/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "Jim"
+    last_name "Bob"
+    email "jimbob@example.com"
+  end
+end
--- test/models/response_test.rb
+++ test/models/response_test.rb
@@ -1,6 +1,18 @@
 require "test_helper"
 
 class ResponseTest < ActiveSupport::TestCase
-  test("coverage") { skip "needs tested" }
+  should belong_to(:event)
+  should belong_to(:user)
 
+  context "The Response class" do
+    should "list responses for upcoming events" do
+      upcoming_event    = create(:event, starts_at: 1.day.from_now)
+      upcoming_response = create(:response, event_id: upcoming_event.id)
+      past_event        = create(:event, starts_at: 1.day.ago)
+      past_response     = create(:response, event_id: past_event.id)
+
+      assert_equal true, Response.upcoming.include?(upcoming_response)
+      assert_equal false, Response.upcoming.include?(past_response)
+    end
+  end
 end
