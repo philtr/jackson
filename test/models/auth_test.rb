7949e8040e23486ebc1c7b75e29cc96c0ff6f813
Phillip Ridlen <phillip@ovenbits.com>
2014-01-29 18:41:33 -0600

    Add tests for event model

--- test/models/auth_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class AuthTest < ActiveSupport::TestCase
-  test("coverage") { skip "needs tested" }
-
-end
-
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,5 +1,44 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  test("coverage") { skip "needs tested" }
+  should have_many(:responses)
+  should have_many(:users).through(:responses)
+  should belong_to(:creator).class_name("User")
+
+  context "The Event class" do
+    should "list events starting in the future" do
+      upcoming_event = create(:event, starts_at: 1.day.from_now)
+      past_event = create(:event, starts_at: 1.day.ago)
+
+      assert_equal true, Event.upcoming.include?(upcoming_event)
+      assert_equal false, Event.upcoming.include?(past_event)
+    end
+  end
+
+  context "An event" do
+    setup do
+      @event = create(:event)
+    end
+
+    should "have a head count" do
+      create_list(:response, 2, event_id: @event.id, additional_guests: 3)
+      assert_equal 8, @event.attending
+    end
+
+    should "find a response for a user" do
+      user = create(:user)
+      response = create(:response, event_id: @event.id, user_id: user.id)
+
+      assert_equal response, @event.response_for(user)
+    end
+
+    should "parameterize all pretty like" do
+      @event.name = "Happiness is a warm gun"
+      assert_equal "#{ @event.id }-happiness-is-a-warm-gun", @event.to_param
+    end
+
+    should "resolve param back to integer" do
+      assert_equal @event, Event.find(@event.to_param)
+    end
+  end
 end
