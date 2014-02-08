8ffa1081ec31ed9fa6326123229e0edcaf9361e5
Phillip Ridlen <phillip@ovenbits.com>
2014-02-08 16:50:10 -0600

    Use SECRET_TOKEN environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Jackson::Application.config.secret_key_base = '82361727d4fbb213b65d13dc071a8a21c26fee40e5cb15a12e747d454c50979c452495e538e6359b650e4b8493b050b84edd7f5e9fb9698e9014fea2630e7762'
+Jackson::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
