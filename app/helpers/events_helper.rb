c607fe566e377a66f1ff0e24f0c7fba07265f9cd
Phillip Ridlen <phillip@ovenbits.com>
2014-08-22 17:35:58 -0500

    Hide name in popover for now. Needs more design thought

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
   def google_maps_link(location)
     link_to location, "https://www.google.com/maps/preview/search/#{ URI.escape(location) }" unless location.nil?
   end
+
+  def name_and_guests(resp)
+    %(#{ resp.user.first_name } #{ resp.user.last_name } <span class="label label-danger">+#{ resp.additional_guests }</span>)
+  end
 end
