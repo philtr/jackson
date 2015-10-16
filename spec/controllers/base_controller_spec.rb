0c93f739ddd13e76a4ed1b8bf44a6cc4efa80286
Phillip Ridlen <phillip@ovenbits.com>
2015-10-16 00:04:48 -0500

    Add specs for BaseController

--- spec/controllers/base_controller_spec.rb
+++ spec/controllers/base_controller_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 describe BaseController do
-  xit "needs to be tested"
+  context "for an authenticated user" do
+    let(:user) { create(:user) }
+
+    before do
+      signin_as(user)
+    end
+
+    it "redirects to the dashboard path" do
+      get :home
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+
+  context "for a signed out user" do
+    describe "GET home" do
+      it "sets the page title" do
+        get :home
+        expect(@controller.send(:page_title)).to eq("Home")
+      end
+
+      it "renders the home template" do
+        expect(get :home).to render_template(:home)
+      end
+    end
+  end
 end
